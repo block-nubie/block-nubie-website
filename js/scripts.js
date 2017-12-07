0c2e6869f04f64c0acb367c5816881bb6fa2dfea
Philip Kirwan <philip@showoff.ie>
2017-12-07 09:15:59 +0000

    Tidy Up JS and Styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,18 @@
-/** Document Ready Functions **/
-/********************************************************************/
-
 $( document ).ready(function() {
-	
-		// Resive video
-		scaleVideoContainer();
-	
-		initBannerVideoSize('.video-container .poster img');
-		initBannerVideoSize('.video-container .filter');
-		initBannerVideoSize('.video-container video');
+		
+	setBannerVideoSize('.video-container .poster img');
+	setBannerVideoSize('.video-container .filter');
+	setBannerVideoSize('.video-container video');
 			
-		$(window).on('resize', function() {
-			// scaleVideoContainer();
-			scaleBannerVideoSize('.video-container .poster img');
-			scaleBannerVideoSize('.video-container .filter');
-			scaleBannerVideoSize('.video-container video');
-		});
-	
+	$(window).on('resize', function() {
+		scaleBannerVideoSize('.video-container .poster img');
+		scaleBannerVideoSize('.video-container .filter');
+		scaleBannerVideoSize('.video-container video');
 	});
 	
-	/** Reusable Functions **/
-	/********************************************************************/
+});
 	
-	function scaleVideoContainer() {
-	
-		// var height = $(window).height();
-		// var unitHeight = parseInt(height) + 'px';
-		// $('.homepage-hero-module').css('height',unitHeight);
-	
-	}
-	
-	function initBannerVideoSize(element){
+	function setBannerVideoSize(element){
 		
 		$(element).each(function(){
 			$(this).data('height', $(this).height());
@@ -48,8 +30,6 @@ $( document ).ready(function() {
 			videoWidth,
 			videoHeight;
 		
-		console.log(windowHeight);
-	
 		$(element).each(function(){
 			var videoAspectRatio = $(this).data('height')/$(this).data('width'),
 				windowAspectRatio = windowHeight/windowWidth;
@@ -67,7 +47,6 @@ $( document ).ready(function() {
 			$(this).width(videoWidth).height(videoHeight);
 	
 			$('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-			
-	
+		
 		});
 	}
