a6fbd03830b9a32ac87853c9830179c28bec5ec6
Sachin Warke <swarke@impesa.net>
2018-08-17 16:01:40 +0530

    New design implementations.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,11 @@ $(document).ready(function () {
 		$("#iframeYoutube").attr("src", "#");
 	})
 
-	var deadline = new Date(Date.parse('05/14/2018'));
-	initializeClock('countdown', deadline);
+	var deadline = new Date(Date.parse('02/28/2019'));
+	// initializeClock('countdown', deadline);
+
+	// var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+  	initializeClock('clockdiv', deadline);
 
 	$('.whitelist').click(function () {
 		dataLayer.push({ 'event': 'Whitelist Click' });
@@ -53,31 +56,6 @@ function getTimeRemaining(endtime) {
 	};
 }
 
-function initializeClock(id, endtime) {
-	var clock = document.getElementById(id);
-	var daysSpan = clock.querySelector('.days');
-	var hoursSpan = clock.querySelector('.hours');
-	var minutesSpan = clock.querySelector('.minutes');
-	var secondsSpan = clock.querySelector('.seconds');
-
-	function updateClock() {
-		var t = getTimeRemaining(endtime);
-
-
-		daysSpan.innerHTML = t.days;
-		hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-		minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-		secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-		if (t.total <= 0) {
-			clearInterval(timeinterval);
-		}
-	}
-
-	updateClock();
-	var timeinterval = setInterval(updateClock, 1000);
-}
-
 function setBannerVideoSize(element) {
 
 	$(element).each(function () {
@@ -116,3 +94,44 @@ function scaleBannerVideoSize(element) {
 
 	});
 }
+
+function getTimeRemaining(endtime) {
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor((t / 1000) % 60);
+	var minutes = Math.floor((t / 1000 / 60) % 60);
+	var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	return {
+	  'total': t,
+	  'days': days,
+	  'hours': hours,
+	  'minutes': minutes,
+	  'seconds': seconds
+	};
+  }
+  
+  function initializeClock(id, endtime) {
+	var clock = document.getElementById(id);
+	var daysSpan = clock.querySelector('.days');
+	var hoursSpan = clock.querySelector('.hours');
+	var minutesSpan = clock.querySelector('.minutes');
+	var secondsSpan = clock.querySelector('.seconds');
+  
+	function updateClock() {
+	  var t = getTimeRemaining(endtime);
+  
+	  daysSpan.innerHTML = t.days;
+	  hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+	  minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+	  secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+	  if (t.total <= 0) {
+		clearInterval(timeinterval);
+	  }
+	}
+  
+	updateClock();
+	var timeinterval = setInterval(updateClock, 1000);
+  }
+  
+  
